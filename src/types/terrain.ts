a9beb566db061b7c256b5f7c0fbef33183618bc2
David Malinen <davidmalinen@outlook.com>
2025-05-01 21:06:34 +0200

    chore: updated types

--- src/types/terrain.ts
+++ src/types/terrain.ts
@@ -5,11 +5,3 @@ export interface TerrainOptions {
   cols: number;
   maxHeight: number;
 }
-
-interface TerrainSystemInterface {
-    getTerrainData(): ElevationMatrix;
-    getElevation(x: number, y: number): number | null;
-    modifyTerrainAt(x: number, y: number, amount: number): void;
-    generateTerrain(generatorType: string, rows: number, cols: number, maxHeight: number): void;
-    clearTerrain(): void;
-}
